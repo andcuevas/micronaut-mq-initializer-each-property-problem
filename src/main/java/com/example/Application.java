0c676dac1506afeb6c0ee15e189c530db9b870a9
Andres Cuevas <andcuevas@gmail.com>
2019-08-16 11:11:16 -0400

    Cleaning...

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -5,8 +5,6 @@ import io.micronaut.runtime.Micronaut;
 public class Application {
 
     public static void main(String[] args) {
-        long pid = ProcessHandle.current().pid();
-        System.setProperty("pid", Long.toString(pid));
         Micronaut.run(Application.class);
     }
 }
